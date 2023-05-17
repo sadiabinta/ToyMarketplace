a963df6cc2e1a2ebe9556076335aaa55015c608d
Sadia Binta Kabir <sadiaa.kabir@gmail.com>
2023-05-17 19:44:48 -0400

    navbar done

--- src/shared/NavBar/NavBar.jsx
+++ src/shared/NavBar/NavBar.jsx
@@ -1,9 +1,46 @@
+import { Link } from "react-router-dom";
+import logo from '../../assets/logo.png'
 
 
 const NavBar = () => {
+    const navItems=<>
+    <li><Link to='/'>Home</Link></li>
+    <li><Link to='/allToys'>All Toys</Link></li>
+    <li><Link to='/myToys'>My Toys</Link></li>
+    <li><Link to='/addToy'>Add a Toy</Link></li>
+    <li><Link to='/blogs'>Blog</Link></li>
+
+    </>
     return (
         <div>
-            <h1>NavBar</h1>
+            <div className="navbar justify-around">
+                <img className="w-24" src={logo} alt="" />
+                <h2 className="text-2xl font-bold text-pink-400 italic">Disney Land Toys</h2>
+                <div>
+                <input type="text" placeholder="Search" className="input input-bordered w-full max-w-xs rounded-l-3xl" />
+                <button className="btn rounded-r-3xl bg-pink-400">Button</button>
+                </div>
+            </div>
+            <div className="navbar bg-sky-700 text-pink-300 font-semibold">
+  <div className="navbar-start">
+    <div className="dropdown">
+      <label tabIndex={0} className="btn btn-ghost lg:hidden">
+        <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h8m-8 6h16" /></svg>
+      </label>
+      <ul tabIndex={0} className="menu menu-compact dropdown-content mt-3 p-2 shadow bg-base-100 rounded-box w-52">
+        {navItems}
+      </ul>
+    </div>
+  </div>
+  <div className="navbar-center hidden lg:flex">
+    <ul className="menu menu-horizontal px-1">
+      {navItems}
+    </ul>
+  </div>
+  <div className="navbar-end">
+    <a className="btn">Get started</a>
+  </div>
+</div>
         </div>
     );
 };

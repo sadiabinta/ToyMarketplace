04cd7ff3429d74178d25878e2d22b789612f5e68
Sadia Binta Kabir <sadiaa.kabir@gmail.com>
2023-05-20 23:35:22 -0400

    aos package used

--- src/pages/Home/Gallery/Gallery.jsx
+++ src/pages/Home/Gallery/Gallery.jsx
@@ -4,7 +4,7 @@ import { useEffect } from "react";
 
 const Gallery = () => {
     useEffect(()=>{
-        
+        Aos.init({duration:'3000'});
     },[])
     return (
         
@@ -12,46 +12,46 @@ const Gallery = () => {
     <h2 className="text-3xl font-semibold text-pink-400 text-center my-5">Gallery</h2>
 <div className="grid grid-cols-2 md:grid-cols-4 gap-4">
     <div className="grid gap-4">
-        <div>
-            <img className="h-auto max-w-full rounded-lg" src="https://flowbite.s3.amazonaws.com/docs/gallery/masonry/image.jpg" alt=""/>
+        <div data-aos='fade-right'>
+            <img className="h-auto max-w-full rounded-lg" src="https://i.ibb.co/nwv9gyD/Car.webp" alt=""/>
         </div>
-        <div>
-            <img className="h-auto max-w-full rounded-lg" src="https://flowbite.s3.amazonaws.com/docs/gallery/masonry/image-1.jpg" alt=""/>
+        <div data-aos='fade-left'>
+            <img className="h-auto max-w-full rounded-lg" src="https://i.ibb.co/M7r8YtN/Plush-toy.webp" alt=""/>
         </div>
-        <div>
-            <img className="h-auto max-w-full rounded-lg" src="https://flowbite.s3.amazonaws.com/docs/gallery/masonry/image-2.jpg" alt=""/>
+        <div data-aos='fade-bottom'>
+            <img className="h-auto max-w-full rounded-lg" src="https://i.ibb.co/Rhb99D9/doll5.jpg" alt=""/>
         </div>
     </div>
-    <div className="grid gap-4">
-        <div>
+    <div className="grid gap-4" >
+        <div data-aos="flip-left">
             <img className="h-auto max-w-full rounded-lg" src="https://flowbite.s3.amazonaws.com/docs/gallery/masonry/image-3.jpg" alt=""/>
         </div>
-        <div>
+        <div data-aos="fade-zoom-in">
             <img className="h-auto max-w-full rounded-lg" src="https://flowbite.s3.amazonaws.com/docs/gallery/masonry/image-4.jpg" alt=""/>
         </div>
-        <div>
+        <div data-aos="fade-up">
             <img className="h-auto max-w-full rounded-lg" src="https://flowbite.s3.amazonaws.com/docs/gallery/masonry/image-5.jpg" alt=""/>
         </div>
     </div>
     <div className="grid gap-4">
-        <div>
+        <div data-aos="flip-left">
             <img className="h-auto max-w-full rounded-lg" src="https://flowbite.s3.amazonaws.com/docs/gallery/masonry/image-6.jpg" alt=""/>
         </div>
-        <div>
+        <div data-aos="fade-up">
             <img className="h-auto max-w-full rounded-lg" src="https://flowbite.s3.amazonaws.com/docs/gallery/masonry/image-7.jpg" alt=""/>
         </div>
-        <div>
+        <div data-aos="flip-right">
             <img className="h-auto max-w-full rounded-lg" src="https://flowbite.s3.amazonaws.com/docs/gallery/masonry/image-8.jpg" alt=""/>
         </div>
     </div>
     <div className="grid gap-4">
-        <div>
+        <div data-aos="flip-left">
             <img className="h-auto max-w-full rounded-lg" src="https://flowbite.s3.amazonaws.com/docs/gallery/masonry/image-9.jpg" alt=""/>
         </div>
-        <div>
+        <div data-aos="fade-zoom-out">
             <img className="h-auto max-w-full rounded-lg" src="https://flowbite.s3.amazonaws.com/docs/gallery/masonry/image-10.jpg" alt=""/>
         </div>
-        <div>
+        <div data-aos="fade-zoom-in">
             <img className="h-auto max-w-full rounded-lg" src="https://flowbite.s3.amazonaws.com/docs/gallery/masonry/image-11.jpg" alt=""/>
         </div>
     </div>
